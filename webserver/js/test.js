7dcf87d8e319748d34d3af8898e38ac0f42b664f
quantbrainz <quantbrainz@gmail.com>
2023-07-19 17:29:59 +0900

    d

---
+++ webserver/js/test.js
@@ -0,0 +1,12 @@
+// var code_lines =document.getElementsByClassName("line");
+
+// for (i=0; i<=5; i++){
+//   code_lines:nth-child(i).style.animation = "typing 2.5s steps(30, end)";
+// }
+
+
+$('#terminal_text').children('line').each(function () {
+    for (i==0; i<=5; i++){
+  i.style.animation = "typing 2.5s steps(30, end)";
+}
+});
